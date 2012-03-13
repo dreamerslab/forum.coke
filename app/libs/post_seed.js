9584d078aae59fc2c267fc3370a76333f8fc9539
mason <mason@dreamerslab.com>
2012-03-13 15:15:32 +0800

    remove all previous posts before injecting posts

--- app/libs/post_seed.js
+++ app/libs/post_seed.js
@@ -6,15 +6,17 @@ var size     = 100;
 
 module.exports = {
   init : function (){
-    for( ; i < size; i++ ){
-      new Post({
-        user_id       : '123456789012345678901234',
-        user_name     : 'Mason Chang',
-        title         : 'Title title title ' + i,
-        content       : 'Content content content ' + i,
-        read_count    : parseInt( i / 10 ),
-        comment_count : parseInt( (99 - i) / 10 )
-      }).save();
-    }
+    Post.collection.drop( function ( err, posts ){
+      for( ; i < size; i++ ){
+        new Post({
+          user_id       : '123456789012345678901234',
+          user_name     : 'Mason Chang',
+          title         : 'Title title title ' + i,
+          content       : 'Content content content ' + i,
+          read_count    : parseInt( i / 10 ),
+          comment_count : parseInt( (99 - i) / 10 )
+        }).save();
+      }
+    });
   }
 };

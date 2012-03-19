2234365c9251413e9dd1e67e8e8d673eb6f8f437
mason <mason@dreamerslab.com>
2012-03-19 20:43:36 +0800

    use node.flow to seed data

--- app/libs/seed.js
+++ app/libs/seed.js
@@ -1,73 +1,124 @@
+var Flow     = require( 'node.flow' );
 var mongoose = require( 'mongoose' );
 var User     = mongoose.model( 'User' );
 var Post     = mongoose.model( 'Post' );
 var Comment  = mongoose.model( 'Comment' );
-
-var users    = [
-  {
-     name  : 'Ben Lin',
-     email : 'ben@dreamerslab.com'
-  } , {
-     name  : 'Fred Chu',
-     email : 'fred@dreamerslab.com'
-  }, {
-     name  : 'Mason Chang',
-     email : 'mason@dreamerslab.com'
-   }
-];
-
+var Cache    = mongoose.model( 'Cache' );
+var flow     = new Flow();
 
 module.exports = {
+
+  users : [
+    {
+       name  : 'Ben Lin',
+       email : 'ben@dreamerslab.com'
+    } , {
+       name  : 'Fred Chu',
+       email : 'fred@dreamerslab.com'
+    }, {
+       name  : 'Mason Chang',
+       email : 'mason@dreamerslab.com'
+     }
+  ],
+
+
   init : function (){
+    var self = this;
 
-    User.collection.drop( function (){
-      Post.collection.drop( function (){
-        Comment.collection.drop( function (){
+    // clearing all collections
+    flow.series( function( next ){
+        User.collection.drop( function (){
+          Post.collection.drop( function (){
+            Comment.collection.drop( function (){
+              Cache.collection.drop( function (){
+                new Cache({
+                  name  : 'sidebar',
+                  trunk : {
+                    tags   : [],
+                    users  : [],
+                    issues : []}
+                }).save( next());
+              }); // end of drop Cache.collection
+            }); // end of drop Comment.collection
+          }); // end of drop Post.collection
+        }); // end of drop User.collection
+    });
 
-          users.forEach( function ( u ){
-            new User( u )
-            .save( function ( err, user ){
+    // creating users
+    this.users.forEach( function ( user ){
+      flow.parallel( function ( user, ready ){
+        new User( user ).save( function ( err, user ){
+          ready();
+        });
+      }, user );
+    });
 
-              var i     = 0;
-              var isize = 10;
+    flow.join();
 
-              for( ; i < isize; i++ ){
-                new Post({
-                  _user      : user._id,
-                  title      : 'Post title',
-                  content    : 'Post content blah blah...',
-                  tags       : [ 'tag1', 'tag2', 'tag3' ],
-                  read_count : Math.floor( Math.random() * 10 ),
-                })
-                .save( function ( err, post ){
+    // creating posts
+    var i = 60;
 
-                  post.update_user( User );
+    for( ; i--; ){
+      flow.parallel( function ( ready ){
+        User.find( function ( err, users ){
+          // get a random user
+          var umax = users.length;
+          var user = users[ Math.floor( Math.random() * umax )];
 
-                  var j     = 0;
-                  var jsize = Math.floor( Math.random() * 10 );
+          new Post({
+            _user      : user._id,
+            title      : 'Post title',
+            content    : 'Post content blah blah...',
+            tags       : [ 'tag1', 'tag2', 'tag3' ],
+            read_count : Math.floor( Math.random() * 10 ),
+          }).save( function ( err, post ){
 
-                  for( ; j < jsize; j++ ){
-                    new Comment({
-                      _user   : user._id,
-                      _post   : post._id,
-                      content : 'Comment content blah blah...'
-                    })
-                    .save( function ( err, comment ){
-
-                      comment.update_user( User );
-                      comment.update_post( Post );
-
-                    });
-                  }
-                });
-              }
-
+            post.update_user( user, function (){
+              ready();
             });
           });
+        });
+      });
+    }
 
-        }); // end of drop Comment.collection
-      }); // end of drop Post.collection
-    }); // end of drop User.collection
+    flow.join();
 
+    // create comments
+    var j = 200;
+
+    for( ; j--; ){
+      flow.parallel( function ( ready ){
+        User.find( function ( err, users ){
+          // get a random user
+          var umax = users.length;
+          var user = users[ Math.floor( Math.random() * umax )];
+
+          Post.find( function ( err, posts ){
+            // get a random post
+            var pmax = posts.length;
+            var post = posts[ Math.floor( Math.random() * pmax )];
+
+            new Comment({
+              _user   : user._id,
+              _post   : post._id,
+              content : 'Comment content blah blah...'
+            }).save( function ( err, comment ){
+              comment.update_user( user, function (){
+                comment.update_post( post, function (){
+                  ready();
+                });
+              });
+            });
+          });
+        });
+      });
+    }
+
+    flow.join();
+
+    flow.end( function (){
+      console.log( 'finished' );
+    });
   }
+
 };
--- app/models/Comment.js
+++ app/models/Comment.js
@@ -3,31 +3,26 @@ var Comment = require( BASE_DIR + 'db/schema' ).Comment;
 
 Comment.methods = {
 
-  update_user : function ( User, callback ){
+  update_user : function ( user, callback ){
     var self = this;
-    User.findById( self._user, function ( err, user ){
-      // Note: add err handling later
-      user.comments.push( self );
-      user.save( function ( err, user ){
-        self.user_name   = user.name;
-        self.user_avatar = user.avatar;
-        self.save( function ( err, comment ){
-          callback && callback.call( self );
-        });
+
+    user.comments.push( this );
+    user.save( function ( err, user ){
+      self.user_name = user.name;
+      self.user_avatar = user.avatar;
+      self.save( function ( err, comment ){
+        callback && callback();
       });
     });
   },
 
-  update_post : function ( Post, callback ){
-    var self = this;
-    Post.findById( self._post, function ( err, post ){
-      // Note: add err handling later
-      post.comments.push( self );
-      post.save( function ( err, post ){
-        // callback && callback.call( self );
-      });
+  update_post : function ( post, callback ){
+    post.comments.push( this );
+    post.save( function ( err, post ){
+      callback && callback();
     });
-  }
+  },
+
 
 };
 

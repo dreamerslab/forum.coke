751a14950e5f1945db6b6d2cc5a30ae2ca797400
mason <mason@dreamerslab.com>
2012-03-21 15:42:09 +0800

    user object can return its attributes

--- app/models/User.js
+++ app/models/User.js
@@ -1,4 +1,16 @@
 var User = require( BASE_DIR + 'db/schema' ).User;
 
 
+User.methods = {
+
+  obj_attrs : function (){
+    return {
+      _id    : this._id,
+      name   : this.name,
+      avatar : this.avatar
+    };
+  }
+
+};
+
 require( 'mongoose' ).model( 'User', User );

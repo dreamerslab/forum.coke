8f36f4573f6edf6c8d525e013321bcfabdd23572
mason <mason@dreamerslab.com>
2012-03-21 18:19:55 +0800

    Tag can extract tag names from a string

--- app/models/Tag.js
+++ app/models/Tag.js
@@ -1,4 +1,23 @@
 var Tag = require( BASE_DIR + 'db/schema' ).Tag;
 
 
+Tag.statics = {
+  extract_names : function ( string ){
+    if( {}.toString.call( string ) !== '[object String]' || string === '' ){
+      return [];
+    }else{
+      var candidates = string.split( /\s*[,|;]\s*/ ).slice( 0, 5 );
+      var names      = [];
+
+      candidates.forEach( function ( name ){
+        name = name.toLowerCase();
+        if( names.indexOf( name ) === -1 && name.length < 20 ){
+          names.push( name );
+        }
+      });
+      return names;
+    }
+  }
+};
+
 require( 'mongoose' ).model( 'Tag', Tag );

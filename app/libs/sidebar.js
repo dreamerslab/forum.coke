be05576af1161d92eec03439447eb4dd8f7a6b36
mason <mason@dreamerslab.com>
2012-03-26 21:47:30 +0800

    show coke's open issues in sidebar

--- app/libs/sidebar.js
+++ app/libs/sidebar.js
@@ -50,6 +50,44 @@ module.exports = {
     });
 
     flow.series( function ( next ){
+      var https   = require( 'https' );
+      var data    = "";
+      var options = {
+        host: 'api.github.com',
+        port: 443,
+        path: '/repos/dreamerslab/coke/issues',
+        method: 'GET'
+      };
+
+      var req = https.request( options, function( res ){
+        res.on( 'data', function ( chunk ){
+          data = data + chunk.toString();
+        });
+
+        res.on( 'end', function (){
+          var issues = JSON.parse( data );
+          issues.forEach( function ( issue ){
+            if( trunk_issues.length < 5 ){
+              if( issue.state === 'open' ){
+                trunk_issues.push({
+                  title : issue.title,
+                  link  : issue.html_url
+                });
+              }
+            }
+          });
+
+          next();
+        });
+      });
+      req.end();
+
+      req.on( 'error', function( err ){
+        console.error( err );
+      });
+    });
+
+    flow.series( function ( next ){
       Cache.findOne({ name : 'sidebar' }, function( err, cache ){
         if( cache ){
           cache = cache;

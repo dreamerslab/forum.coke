f2257e459fc0be58c926210021af580220f137cc
mason <mason@dreamerslab.com>
2012-04-02 17:39:56 +0800

    user must get authenticated to post a comment

--- app/controllers/comments.js
+++ app/controllers/comments.js
@@ -7,30 +7,32 @@ var Application = require( CONTROLLER_DIR + 'application' );
 module.exports = Application.extend({
 
   create : function ( req, res, next ){
-    // Note: should replace this user by session user later
-    User.findOne( function ( err, user ){
-      Post.findById( req.params.id, function ( err, post ){
+    // redirect to 'posts/show' if not authenticated
+    if( !req.user ){
+      res.redirect( '/posts/show' );
+      return;
+    }
 
-        var comment = new Comment({
-          user    : user,
-          post    : post,
-          content : req.body.comment.content
-        });
+    Post.findById( req.params.id, function ( err, post ){
+      var user = req.user;
+      var comment = new Comment({
+        user    : user,
+        post    : post,
+        content : req.body.comment.content
+      });
 
-        comment.save( function ( err, comment ){
-          if( err ){
-            next( err );
-            return;
-          }
+      comment.save( function ( err, comment ){
+        if( err ){
+          next( err );
+          return;
+        }
 
-          comment.add_to_post( post );
-          comment.add_to_user( user );
+        comment.add_to_post( post );
+        comment.add_to_user( user );
 
-          req.flash( 'flash-info', 'Comment created' );
-          res.redirect( '/posts/' + post._id );
-        });
+        req.flash( 'flash-info', 'Comment created' );
+        res.redirect( '/posts/' + post._id );
       });
     });
-
   },
 });

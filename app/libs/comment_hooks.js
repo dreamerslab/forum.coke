7464a26517793ecffc2560774f7d3c5636752e76
mason <mason@dreamerslab.com>
2012-04-05 14:00:48 +0800

    prevent duplicate comment_id in user and toppic

--- app/libs/comment_hooks.js
+++ app/libs/comment_hooks.js
@@ -19,30 +19,58 @@ module.exports = {
   },
 
   post_save : function (){
+    var self  = this;
     var User  = mongoose.model( 'User' );
     var Topic = mongoose.model( 'Topic' );
 
     // add comment's _id to its user
-    User.update(
-      { _id : this.user },
-      { $push : { comments : this._id }},
-      function ( err ){
-        if( err ){
-          next( err );
-          return;
-        }
-      });
+    User.findById( this.user, function ( err, user ){
+      if( err ){
+        // next( err );
+        // NOTE: there is not 'next' in mongoose's
+        // post middleware, how can I handle error?
+        return;
+      }
+
+      if( user.comments.indexOf( self._id ) === -1 ){
+        User.update(
+          { _id : self.user },
+          { $push : { comments : self._id }},
+          function ( err ){
+            if( err ){
+              // next( err );
+              // NOTE: there is not 'next' in mongoose's
+              // post middleware, how can I handle error?
+              return;
+            }
+          });
+      }
+    });
 
     // add comment's _id to its topic
-    Topic.update(
-      { _id : this.topic },
-      { $push : { comments : this._id }},
-      function ( err ){
-        if( err ){
-          next( err );
-          return;
-        }
-      });
+    Topic.findById( this.topic, function ( err, topic ){
+      if( err ){
+        // next( err );
+        // NOTE: there is not 'next' in mongoose's
+        // post middleware, how can I handle error?
+        return;
+      }
+
+      if( topic.comments.indexOf( self._id ) === -1 ){
+        Topic.update(
+          { _id : self.topic },
+          { $push : { comments : self._id }},
+          function ( err ){
+            if( err ){
+              next( err );
+              // next( err );
+              // NOTE: there is not 'next' in mongoose's
+              // post middleware, how can I handle error?
+              return;
+            }
+          });
+      }
+    });
   },
 
   pre_remove : function ( next ){

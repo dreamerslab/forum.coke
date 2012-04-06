2a9cf25f84b47becbd3267ca8f51e0e016f856df
mason <mason@dreamerslab.com>
2012-04-06 15:08:07 +0800

    add obj_attrs() to Topic

--- app/models/Topic.js
+++ app/models/Topic.js
@@ -2,7 +2,6 @@ var Topic = require( BASE_DIR + 'db/schema' ).Topic;
 var Flow  = require( 'node.flow' );
 
 Topic.statics = {
-
   paginate : function ( conds, opts, next, callback ){
     var reslut = {};
     var self   = this;
@@ -34,6 +33,12 @@ Topic.statics = {
 };
 
 Topic.methods = {
+  obj_attrs : function (){
+    return {
+      _id   : this._id,
+      title : this.title
+    };
+  },
 
   inc_read_count : function (){
     this.read_count = this.read_count + 1;

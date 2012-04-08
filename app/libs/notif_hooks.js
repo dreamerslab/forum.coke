3e1842dcce5391f29de7a09b64f6e1daeabeb71e
mason <mason@dreamerslab.com>
2012-04-08 13:25:42 +0800

    comment the unused code

--- app/libs/notif_hooks.js
+++ app/libs/notif_hooks.js
@@ -1,14 +1,14 @@
 var mongoose = require( 'mongoose' );
-var Message  = {
-  'update-topic'   : 'updated the topic',
-  'create-comment' : 'commented on the topic'
-};
+// var Message  = {
+//   'update-topic'   : 'updated the topic',
+//   'create-comment' : 'commented on the topic'
+// };
 
 
 
 module.exports = {
   pre_save : function ( next ){
-    this.activity = Message[ this.type ];
+    // this.activity = Message[ this.type ];
     next();
   },
 

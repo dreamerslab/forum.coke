6396426e09143d51c27d28313a1dbef601291db1
mason <mason@dreamerslab.com>
2012-05-02 16:02:11 +0800

    paginate users index page

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,20 +5,39 @@ var Application = require( CONTROLLER_DIR + 'application' );
 
 
 module.exports = Application.extend({
+  _merge : function ( req, result, base_query ){
+    return UTILS.merge( result || {}, {
+      sidebar   : req.sidebar,
+      sess_user : req.user,
+      path      : req.path,
+      query     : base_query || ''
+    });
+  },
+
   init : function ( before, after ){
     before( this.fill_sidebar );
   },
 
   index : function ( req, res, next ){
-    User.find( function ( err, users ){
-      if( err ) return next( err );
+    var self  = this;
+    var conds = {};
+    var opts  = { sort  : [ 'updated_at', -1 ],
+                  skip  : req.query.from || 0,
+                  limit : 20 };
 
-      res.render( 'users/index', {
-        sidebar   : req.sidebar,
-        sess_user : req.user,
-        users     : users
-      });
+    User.paginate( conds, opts, next, function ( result ){
+      res.render( 'users/index', self._merge( req, result, '?' ));
     });
+
+    // User.find( function ( err, users ){
+    //   if( err ) return next( err );
+
+    //   res.render( 'users/index', {
+    //     sidebar   : req.sidebar,
+    //     sess_user : req.user,
+    //     users     : users
+    //   });
+    // });
   },
 
   show : function ( req, res, next ){
--- app/models/User.js
+++ app/models/User.js
@@ -3,6 +3,30 @@ var User = require( BASE_DIR + 'db/schema' ).User;
 
 
 User.statics = {
+  paginate : function ( conds, opts, next, callback ){
+    var reslut = {};
+    var self   = this;
+
+    this.count( conds, function ( err, count ){
+      if( err ) return next( err );
+
+      self.
+        find( conds ).
+        sort( opts.sort[ 0 ], opts.sort[ 1 ]).
+        skip( opts.skip ).
+        limit( opts.limit ).run( function ( err, users ){
+          if( err ) return next( err );
+
+          callback && callback({
+            users  : users,
+            count  : count,
+            from   : opts.skip,
+            limit  : opts.limit
+          });
+        });
+    });
+  },
+
   push_topic : function ( topic, callback ){
     this.findById( topic.user, function ( err, user ){
       if( err ) return callback && callback( err );

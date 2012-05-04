ef04c7c2f697b4c70caddc979fb221e55e3fce60
Fred Chu <cjw3112386@gmail.com>
2012-05-04 11:48:33 +0800

    front init, added sass, imgs, config.rb, start to modified views

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/css"
+http_stylesheets_path = "/css"
+sass_dir = "app/sass"
+images_dir = "public/img"
+http_images_path = "/img"
+javascripts_dir = "public/js"
+preferred_syntax = :sass
+output_style = :expanded
+environment = :development
+# output_style = :compressed
+# environment = :production
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true

9ad8e848a2296981e4ecfc23bc8630c889a23629
mason <mason@dreamerslab.com>
2012-05-07 12:11:23 +0800

    add code comment for notif hooks

--- app/models/hooks/notif.js
+++ app/models/hooks/notif.js
@@ -1,6 +1,8 @@
 var mongoose = require( 'mongoose' );
 
 module.exports = {
+  // Debugging notification messages
+  // for development use only
   post_save : function (){
     // var self = this;
     // var User = mongoose.model( 'User' );

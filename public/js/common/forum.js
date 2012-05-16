baabf87b814e81842e7b88c7b858d54d5c7afe25
Fred Chu <cjw3112386@gmail.com>
2012-05-16 21:49:08 +0800

    fixed a lot of css bug and modified html

--- public/js/common/forum.js
+++ public/js/common/forum.js
@@ -1,5 +1,20 @@
 ( function ( $ ){
   $( function (){
     $( 'pre' ).addClass( 'prettyprint' );
+
+    var $a = $( 'a, button, input[type="submit"], input[type="reset"]' );
+
+    $a.click( function (){
+      this.blur();
+    }).
+    mousedown( function (){
+      this.blur();
+    });
+
+    if( /msie/i.test( navigator.userAgent ) && !/opera/i.test( navigator.userAgent )){
+      $a.focus( function (){
+        this.blur();
+      });
+    }
   });
 })( jQuery );

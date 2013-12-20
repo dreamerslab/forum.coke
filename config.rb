5e6cf245f6ebea9c07fccd2c16a995df5a458fea
ben <ben@dreamerslab.com>
2013-12-19 17:46:52 -0800

    update http_generated_images_path for newer version of compass

--- config.rb
+++ config.rb
@@ -1,14 +1,14 @@
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "public/css"
-http_stylesheets_path = "/css"
-sass_dir = "app/sass"
-images_dir = "public/img"
-http_images_path = "/img"
-javascripts_dir = "public/js"
-preferred_syntax = :sass
-output_style = :expanded
-environment = :development
+http_path                  = "/"
+css_dir                    = "public/css"
+http_stylesheets_path      = "/css"
+sass_dir                   = "app/sass"
+images_dir                 = "public/img"
+http_generated_images_path = "/img"
+javascripts_dir            = "public/js"
+preferred_syntax           = :sass
+output_style               = :expanded
+environment                = :development
 # output_style = :compressed
 # environment = :production
 # To enable relative paths to assets via compass helper functions. Uncomment:
